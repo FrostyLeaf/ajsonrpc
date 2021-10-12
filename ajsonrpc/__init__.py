dfe7fe914e14980cbec3111229fc573ad942ede1
leaf4monkey <leaf4monkey@gmail.com>
2021-10-13 04:40:29 +0800

    Update version

--- ajsonrpc/__init__.py
+++ ajsonrpc/__init__.py
@@ -1,4 +1,4 @@
 from .dispatcher import Dispatcher
 from .manager import AsyncJSONRPCResponseManager
 
-__version__ = "0.0.0"  # replaced with release tag in GitHub action
+__version__ = "1.2.2"  # replaced with release tag in GitHub action
